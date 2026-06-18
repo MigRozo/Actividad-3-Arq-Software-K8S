d15911b8a893485c39aea6a53ba3614592a7290e
Mig <angelorozo13@hotmail.com>
2026-06-18 13:37:41 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.json({
+		message: 'microservicio para el diplomado de Arquitectura de Software.',
+		status: 'OK',
+	})
+});
+
+app.listen(port, () => {
+	console.log(`Microservicio ejecutándose en el puerto ${port}`);
+});
